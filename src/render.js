2eac3552d995ef2efc013847763d1dae8d28ac6d
loops <128599880+loopsc@users.noreply.github.com>
2025-08-03 16:54:32 +1200

    Added axis headings and buttons on the side.

--- src/render.js
+++ src/render.js
@@ -1,6 +1,21 @@
 export function setupScreen() {
     const main = document.getElementById("main");
 
+    const boardWrapper = document.createElement("div");
+    boardWrapper.classList.add("board-wrapper");
+
+    const corner = document.createElement("div");
+    corner.classList.add("corner-cell");
+
+    const colLabels = document.createElement("div");
+    colLabels.classList.add("col-labels");
+
+    const rowLabels = document.createElement("div");
+    rowLabels.classList.add("row-labels");
+
+    const buttonsMenu = document.createElement("div");
+    buttonsMenu.classList.add("buttons-menu");
+
     const board = document.createElement("div");
     board.classList.add("board");
 
@@ -10,14 +25,77 @@ export function setupScreen() {
     playerNameInput.classList.add("player-name-input");
 
     const startGameButton = document.createElement("button");
-    startGameButton.classList.add("button");
+    startGameButton.classList.add("menu-buttons");
     startGameButton.textContent = "Start Game";
 
+    generateButtons(buttonsMenu);
+
+    boardWrapper.appendChild(corner);
+    boardWrapper.appendChild(colLabels);
+    boardWrapper.appendChild(buttonsMenu);
+    boardWrapper.appendChild(rowLabels);
+    boardWrapper.appendChild(board);
+
     main.appendChild(playerNameInput);
-    main.appendChild(board);
+    main.appendChild(boardWrapper);
     main.appendChild(startGameButton);
 
+    generateAxisLabels(colLabels, rowLabels);
     generateGrid(board);
+    
+}
+
+function generateButtons(container) {
+    const carrierButton = document.createElement("button");
+    carrierButton.classList.add("menu-buttons", "ship-button");
+    carrierButton.textContent = "Carrier";
+
+    const battleshipButton = document.createElement("button");
+    battleshipButton.classList.add("menu-buttons", "ship-button");
+    battleshipButton.textContent = "Battleship"
+
+    const cruiserButton = document.createElement("button");
+    cruiserButton.classList.add("menu-buttons", "ship-button");
+    cruiserButton.textContent = "Cruiser"
+
+    const subButton = document.createElement("button");
+    subButton.classList.add("menu-buttons", "ship-button");
+    subButton.textContent = "Submarine"
+
+    const destroyerButton = document.createElement("button");
+    destroyerButton.classList.add("menu-buttons", "ship-button");
+    destroyerButton.textContent = "Destroyer"
+
+    const orientationButton = document.createElement("button");
+    orientationButton.classList.add("menu-buttons","ship-button", "orientation-button");
+    orientationButton.textContent = "Horizontal";
+    orientationButton.addEventListener("click", () => {
+        orientationButton.textContent = orientationButton.textContent === "Horizontal" ? "Vertical" : "Horizontal";
+    })
+
+
+    container.append(
+        carrierButton,
+        battleshipButton,
+        cruiserButton,
+        subButton,
+        destroyerButton,
+        orientationButton
+    );
+}
+
+function generateAxisLabels(colContainer, rowContainer) {
+    for (let i = 0; i < 10; i++) {
+        const colLabel = document.createElement("div");
+        colLabel.textContent = String.fromCharCode(65 + i); // A–J
+        colLabel.classList.add("axis-text");
+        colContainer.appendChild(colLabel);
+
+        const rowLabel = document.createElement("div");
+        rowLabel.textContent = i + 1; // 1–10
+        rowLabel.classList.add("axis-text");
+        rowContainer.appendChild(rowLabel);
+    }
 }
 
 function generateGrid(container, gridSize = 10) {
